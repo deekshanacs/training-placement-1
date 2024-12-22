ae2b063babf2039960820a30dd787c6adc892bac
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2024-12-22 19:46:12 +0530

    HCK-2.java

---
+++ 22.12.2024/HCK-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next(); 
+        sc.close();
+        String B= new StringBuilder(A).reverse().toString();
+        if (A.equals(B)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
+
+
